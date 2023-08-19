ac9890dc28c3fa958e0731bff5ea73d9db0e39a1
JessicaRBailey <129994268+JessicaRBailey@users.noreply.github.com>
2023-08-18 22:21:07 -0700

    WeatherPy complete

---
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,14 @@
+# Add your API key  for weather
+weather_api_key = "41244c2ac018a7276e9ff10e564aa9a2"
+
+#Census Data API key:
+census_api_key = "33ed5aac0cd4d7b4e8558a21e8f7628d6e1782c5"
+
+#Geoapify API key:
+geoapify_key = "65f21560d49c407d96a6f0147067e680"
+
+# NY Times API key:
+
+
+#National Park Service API key:
+NPSapi_key = "8eEB9uf4zOxc3qfcrUkKtm7mM1ZqIGbfsWLy3oRh"
